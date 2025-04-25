9e5aba91dc4ebdfcf6df638b5286356e8137eb6a
edularson <eduardolarson05@gmail.com>
2025-04-25 13:55:48 -0300

    new commit

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,8 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
-import { error } from "console";
-import AppError from "@shared/errors/AppError";
+import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 
 const app = express();
@@ -11,19 +10,19 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-    if(error instanceof AppError){
-        return response.status(error.statusCode).json({
-            status: 'error',
-            message: error.message
+app.use((error: Error, request : Request, response : Response, next : NextFunction) =>{
+        if(error instanceof AppError){
+            return response.status(error.statusCode).json({
+                status:'error',
+                message: error.message
+            });
+        }
+        return response.status(500).json({
+            status:'error',
+            message: 'Internal Server Error'
         });
-    }
-    return response.status(500).json({
-        status: 'error',
-        message: 'Internal Server Error'
-    });
 });
 
 app.listen(3333, () => {
     console.log('Server started on port 3333');
-})
+});
